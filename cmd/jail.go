8a9cb321bc94852c3d59b02651b9f426290c3449
motylkov <motylkov@yandex.ru>
2025-01-23 22:28:52 +0300

    upgrade jail implementation

--- cmd/jail.go
+++ cmd/jail.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"FreeBSD-Command-manager/internal"
+	"FreeBSD-Command-manager/internal/jail"
 	"fmt"
 	"os"
 
@@ -19,23 +20,27 @@ var jailCreateCmd = &cobra.Command{
 	Use:   "create",
 	Short: "Create a new jail",
 	Run: func(cmd *cobra.Command, args []string) {
-		cfg := internal.JailConfig{
+		manager := jail.DefaultManager()
+
+		cfg := jail.Config{
 			Name:  jailName,
 			Path:  jailPath,
 			IP:    jailIP,
 			Mount: jailMount,
 		}
-		if err := internal.CreateJail(cfg); err != nil {
+
+		if err := manager.Create(cfg); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+
 		result := map[string]interface{}{
 			"jail_id": jailName,
 			"status":  "created",
 			"ip":      jailIP,
-			"network": "br-jails", // TODO: make dynamic if needed
+			"network": "br-jails", // make dynamic ?
 		}
-		if err := internal.OutputTerraform(result); err != nil {
+		if err := internal.Output(result); err != nil {
 			os.Exit(1)
 		}
 	},
@@ -45,15 +50,18 @@ var jailStartCmd = &cobra.Command{
 	Use:   "start",
 	Short: "Start a jail",
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := internal.StartJail(jailName); err != nil {
+		manager := jail.DefaultManager()
+
+		if err := manager.Start(jailName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+
 		result := map[string]interface{}{
 			"jail_id": jailName,
 			"status":  "started",
 		}
-		if err := internal.OutputTerraform(result); err != nil {
+		if err := internal.Output(result); err != nil {
 			os.Exit(1)
 		}
 	},
@@ -63,15 +71,18 @@ var jailStopCmd = &cobra.Command{
 	Use:   "stop",
 	Short: "Stop a jail",
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := internal.StopJail(jailName); err != nil {
+		manager := jail.DefaultManager()
+
+		if err := manager.Stop(jailName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+
 		result := map[string]interface{}{
 			"jail_id": jailName,
 			"status":  "stopped",
 		}
-		if err := internal.OutputTerraform(result); err != nil {
+		if err := internal.Output(result); err != nil {
 			os.Exit(1)
 		}
 	},
@@ -81,15 +92,61 @@ var jailDestroyCmd = &cobra.Command{
 	Use:   "destroy",
 	Short: "Destroy a jail",
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := internal.DestroyJail(jailName); err != nil {
+		manager := jail.DefaultManager()
+
+		if err := manager.Destroy(jailName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+
 		result := map[string]interface{}{
 			"jail_id": jailName,
 			"status":  "destroyed",
 		}
-		if err := internal.OutputTerraform(result); err != nil {
+		if err := internal.Output(result); err != nil {
+			os.Exit(1)
+		}
+	},
+}
+
+var jailListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all jails",
+	Run: func(cmd *cobra.Command, args []string) {
+		manager := jail.DefaultManager()
+
+		jails, err := manager.List()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		result := map[string]interface{}{
+			"jails": jails,
+			"count": len(jails),
+		}
+		if err := internal.Output(result); err != nil {
+			os.Exit(1)
+		}
+	},
+}
+
+var jailInfoCmd = &cobra.Command{
+	Use:   "info",
+	Short: "Get information about a jail",
+	Run: func(cmd *cobra.Command, args []string) {
+		manager := jail.DefaultManager()
+
+		info, err := manager.GetInfo(jailName)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		result := map[string]interface{}{
+			"jail_info": info,
+		}
+		if err := internal.Output(result); err != nil {
 			os.Exit(1)
 		}
 	},
@@ -117,11 +174,17 @@ func init() {
 	jailDestroyCmd.Flags().StringVar(&jailName, "name", "", "Jail name (required)")
 	jailDestroyCmd.MarkFlagRequired("name")
 
+	// Info command flags
+	jailInfoCmd.Flags().StringVar(&jailName, "name", "", "Jail name (required)")
+	jailInfoCmd.MarkFlagRequired("name")
+
 	// Add all subcommands
 	jailCmd.AddCommand(jailCreateCmd)
 	jailCmd.AddCommand(jailStartCmd)
 	jailCmd.AddCommand(jailStopCmd)
 	jailCmd.AddCommand(jailDestroyCmd)
+	jailCmd.AddCommand(jailListCmd)
+	jailCmd.AddCommand(jailInfoCmd)
 
 	cmd.AddCommand(jailCmd)
 }
